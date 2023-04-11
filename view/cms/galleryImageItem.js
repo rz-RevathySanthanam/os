9ba572687ee48c530c14bbaa630fc4584a02f8de
rz-RevathySanthanam <revathy.santhanam@roanuz.com>
2023-04-11 09:17:18 +0530

    Updated Landing Page Changes

--- view/cms/galleryImageItem.js
+++ view/cms/galleryImageItem.js
@@ -1,44 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withDependencySupport } from '@/roanuz/lib/dep';
 import styled from 'styled-components';
-import Link from 'next/link';
-import { TextMedium16 } from '@/roanuz/typopgraphy';
-import { DatoResponsiveImageView } from './datoImageView';
 
-const GalleryImageItemWrapper = styled.div`
+export const BaseGalleryImageItemLayoutWrapper = styled.div`
 `;
 
-export const GalleryImageItem = ({ item }) => {
-  return (
-    <GalleryImageItemWrapper className={`${item.cssClass} gallery-image-item`}>
-      {item.link
-        ? (
-          <Link href={item.link}>
-            <a>
-              <DatoResponsiveImageView item={item} />
-              {item.title && (
-                <div>
-                  <TextMedium16>{item.title}</TextMedium16>
-                </div>
-              )}
-            </a>
-          </Link>
-        )
-        : (
-          <>
-            <DatoResponsiveImageView item={item} />
-          </>
-        )}
-    </GalleryImageItemWrapper>
-  );
+export const BaseGalleryImageItemLayout = ({
+  media, title, subtitle, description, link,
+}) => (
+  <GalleryImageItemLayoutWrapper>
+    <div className="banner-media">
+      {media && (
+        <>
+          {media}
+        </>
+      )}
+    </div>
+    <div className="banner-content">
+      {title && (
+        <>
+          {title}
+        </>
+      )}
+      {subtitle && (
+        <>
+          {subtitle}
+        </>
+      )}
+      {description && (
+        <>
+          {description}
+        </>
+      )}
+      {link && (
+        <div className="banner-link">
+          {link}
+        </div>
+      )}
+    </div>
+  </GalleryImageItemLayoutWrapper>
+);
+
+BaseGalleryImageItemLayout.propTypes = {
+  media: PropTypes.element,
+  title: PropTypes.element,
+  subtitle: PropTypes.element,
+  description: PropTypes.element,
+  link: PropTypes.element,
 };
 
-GalleryImageItem.propTypes = {
-  item: PropTypes.objectOf(PropTypes.shape({
-    link: PropTypes.string,
-    cssClass: PropTypes.string,
-    image: PropTypes.object.isRequired,
-    imageTablet: PropTypes.object,
-    imageMobile: PropTypes.object,
-  })).isRequired,
-};
+export const GalleryImageItemLayout = withDependencySupport(BaseGalleryImageItemLayout, 'GalleryImageItemLayout');
+export const GalleryImageItemLayoutWrapper = withDependencySupport(BaseGalleryImageItemLayoutWrapper, 'GalleryImageItemLayoutWrapper');
