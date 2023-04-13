02e106b306f5833af4ad02b9316f7636f4377a20
rz-Revathy Santhanam <96763144+rz-RevathySanthanam@users.noreply.github.com>
2023-04-13 17:58:09 +0530

    Delete view directory

--- view/footer/certifyMarks.js
+++
@@ -1,8 +0,0 @@
-import { withDependencySupport } from '@/roanuz/lib/dep';
-
-export const BaseCertifyMarksView = () => {
-  return null;
-};
-
-BaseCertifyMarksView.propTypes = {};
-export const CertifyMarksView = withDependencySupport(BaseCertifyMarksView, 'CertifyMarksView');
